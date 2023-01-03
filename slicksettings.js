120a860063b41d3cbf0bd2ecc862220c48262f3e
toyamvcieq <121881306+toyamvcieq@users.noreply.github.com>
2023-01-03 16:11:07 +0100

    Add files via upload

---
+++ slicksettings.js
@@ -0,0 +1,25 @@
+$('.team-carousel').slick({
+	mobileFirst: true,
+	autoplay: true,
+	arrows: false,
+
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+			},
+		},
+	],
+});
